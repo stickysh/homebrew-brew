5fd450e9f4be8a8f52c5704d0123ef26a644ce3a
Eddy k <edkvmn@gmail.com>
2021-02-01 22:34:22 -0500

    Update sticky.rb

--- Formula/sticky.rb
+++ Formula/sticky.rb
@@ -1,6 +1,9 @@
 class Sticky < Formula
 	desc "Use this to build automation te ease your life" 
 	homepage "https://sticky.sh"
+	url "https://assets.sticky.sh/cli/sticky-v0.9-alpha/sticky-v0.9-alpha.tar.xz"
+	sha256 "d250edee5a099a19746d75ecf9b697296323fa4d8344ea0c7e9a699bf58ba797"
+	
 	def install
 	end
 end
