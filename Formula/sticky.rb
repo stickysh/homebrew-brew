b3ed21ee6ba2611ea209f13f71014e3168deb1ee
edkvm <edkvmn@gmail.com>
2021-02-02 20:12:06 -0500

    fix url

--- Formula/sticky.rb
+++ Formula/sticky.rb
@@ -1,7 +1,7 @@
 class Sticky < Formula
 	desc "Use this to build automation te ease your life" 
 	homepage "https://sticky.sh"
-	url "https://assets.sticky.sh/cli/sticky-v0.9.0/sticky-v0.9.tar.xz"
+	url "https://assets.sticky.sh/cli/sticky-v0.9.0/sticky-v0.9.0.tar.xz"
 	sha256 "bc5fe8ae2759725ad67f5cfef0d87bdd5b4a49a5ec1095c138798509c0c47188"
 	
 	def install
