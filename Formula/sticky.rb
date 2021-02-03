d6298cd46e47fc864b4ffe043e308a3642bc8553
edkvm <edkvmn@gmail.com>
2021-02-02 21:59:39 -0500

    added go build

--- Formula/sticky.rb
+++ Formula/sticky.rb
@@ -1,11 +1,11 @@
 class Sticky < Formula
 	desc "Use this to build automation te ease your life" 
 	homepage "https://sticky.sh"
-	url "https://assets.sticky.sh/cli/sticky-v0.9.0/sticky-v0.9.0.tar.xz"
-	sha256 "bc5fe8ae2759725ad67f5cfef0d87bdd5b4a49a5ec1095c138798509c0c47188"
+	url "https://assets.sticky.sh/cli/sticky-v0.9.0/sticky-v0.9.0.tar.gz"
+	sha256 "58bc3a4c40e3473fb335e098d7d8bf1965ad332b69f2bad3617c62e3ef942656"
 	
 	def install
-	    bin.install "bin/sticky"
+	    system "go", "build", *std_go_args, "-ldflags", "-s -w"
   	end
 	
 	test do
