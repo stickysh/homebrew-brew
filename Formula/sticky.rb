b1c04b8360c8c67ebb9a99872849ec954b008054
edkvm <edkvmn@gmail.com>
2021-02-02 22:02:39 -0500

    added depend

--- Formula/sticky.rb
+++ Formula/sticky.rb
@@ -3,6 +3,8 @@ class Sticky < Formula
 	homepage "https://sticky.sh"
 	url "https://assets.sticky.sh/cli/sticky-v0.9.0/sticky-v0.9.0.tar.gz"
 	sha256 "58bc3a4c40e3473fb335e098d7d8bf1965ad332b69f2bad3617c62e3ef942656"
+
+	depends_on "go" => :build
 	
 	def install
 	    system "go", "build", *std_go_args, "-ldflags", "-s -w"
